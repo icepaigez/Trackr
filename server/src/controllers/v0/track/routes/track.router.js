60340684ca440381cb5582b27d7a6e4e0a42b6eb
tunde <icepaigez@gmail.com>
2024-06-28 21:30:44 +0100

    feat:add tracking data
    added new data for each tracking point for parcels

--- server/src/controllers/v0/track/routes/track.router.js
+++ server/src/controllers/v0/track/routes/track.router.js
@@ -30,13 +30,24 @@ router.get('/generate', async(req, res) => {
 })
 
 router.post('/update', async(req, res) => {
-	const { trackingNumber } = req.body;
+	const { trackingNumber, details, location, status } = req.body;
 	try {
 		const trackingNumberRef = db.ref('trackingNumbers/' + trackingNumber);
 		trackingNumberRef.once('value', snapshot => {
 			const data = snapshot.val();
-			console.log(data)
-			res.sendStatus(200)
+			if (data) {
+				const dataPoint = {}
+				const currentStatus = {};
+				const dateTimeEpoch = JSON.stringify(Date.parse(new Date()));
+				dataPoint['details'] = details;
+				dataPoint['location'] = location;
+				dataPoint['status'] = status;
+				currentStatus[dateTimeEpoch] = dataPoint;
+				db.ref('trackingNumbers/' + trackingNumber).update(currentStatus)
+				res.sendStatus(200);
+			} else {
+				res.status(404).send({message:'Tracking number not found'})
+			}
 		})
 	} catch(error) {
 		console.log('Error when updating package status', err)
