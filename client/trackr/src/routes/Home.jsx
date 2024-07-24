57d24325703395120ad46d1ec167d35c44d13a3f
tunde <icepaigez@gmail.com>
2024-07-24 09:51:52 +0100

    new navbar
    added a second navbar for more options

--- client/trackr/src/routes/Home.jsx
+++ client/trackr/src/routes/Home.jsx
@@ -1,12 +1,14 @@
 import Header from './components/home/Header'
 import Hero from './components/home/Hero'
 import Footer from './components/home/Footer'
+import Navbar from './components/home/NavBar'
 
 function Home() {
   return (
     <div className="relative flex size-full min-h-screen flex-col bg-white group/design-root overflow-x-hidden" style={{ fontFamily: '"Work Sans", "Noto Sans", sans-serif' }}>
       <div className="layout-container flex h-full grow flex-col rounded-2xl">
         <Header />
+        <Navbar />
         <Hero />
         <Footer />
       </div>
@@ -14,4 +16,4 @@ function Home() {
   )
 }
 
-export default Home
+export default Home 
